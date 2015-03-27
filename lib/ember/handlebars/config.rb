507f4e1a9945c38880e4eef4fa7fba976d874f91
Ryunosuke SATO <tricknotes.rs@gmail.com>
2015-03-28 01:26:19 +0900

    Remove unused method

--- lib/ember/handlebars/config.rb
+++ lib/ember/handlebars/config.rb
@@ -1,16 +1,12 @@
 module Ember
   module Handlebars
     class Config
-      OPTIONS = [
-        :precompile,
+      attr_accessor :precompile,
         :ember_template,
         :output_type,
         :amd_namespace,
         :templates_root,
         :templates_path_separator
-      ].freeze
-
-      attr_accessor *OPTIONS
 
       def initialize
         self.precompile = true
@@ -19,10 +15,6 @@ module Ember
         self.amd_namespace = nil
         self.templates_root = 'templates'
         self.templates_path_separator = '/'
-      end
-
-      def to_hash
-        Hash[OPTIONS.map {|option| [option, __send__(option)] }]
       end
     end
   end
